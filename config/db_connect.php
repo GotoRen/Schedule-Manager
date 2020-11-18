a04a058f528f5573d1e9be780d9faa96e537784e
GotoRen <r0719en@pluslab.org>
2020-11-18 13:54:38 +0900

    [Update]: Class DBConnecter.

--- config/db_connect.php
+++ config/db_connect.php
@@ -14,11 +14,11 @@ class DBConnecter {
     private function db_connect() {
         try {
             $this->pdo = new PDO(_DSN, _DB_USER, _DB_PASS);
-          	$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         } catch(PDOException $Exception) {
-			echo "エラー：データベースに接続できません";
-          	die('エラー :' . $Exception->getMessage());
+	    echo "エラー：データベースに接続できません";
+            die('エラー :' . $Exception->getMessage());
         }
     }
 }
